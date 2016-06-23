db3052ecaa1b2821ed20874225a3ddc0966f5e5f
Adam Meghji <adam@meghji.org>
2016-06-23 19:04:24 -0400

    Add support for Discovery API v2 Search Attractions

---
+++ test/discovery/v2/attraction/all.js
@@ -0,0 +1,25 @@
+var chai      = require("chai");
+var should    = chai.should();
+var nock      = require('nock');
+var nockBack  = nock.back;
+var All       = require('../../../../lib/discovery/v2/attraction/all');
+
+describe('discovery.v2.attraction.all', function() {
+  before(function() {
+    nockBack.fixtures = './test/fixtures/discovery/v2'
+  });
+
+  describe('success', function() {
+    it('should find an attraction', function(done) {
+      nockBack('attraction/all-200.json', {}, function(nockDone) {
+        var all = All('mock-api-key');
+        all()
+        .then(function(attractions) {
+          attractions[0].name.should.equal("!!!");
+          nockDone();
+          done();
+        })
+      });
+    });
+  });
+});
