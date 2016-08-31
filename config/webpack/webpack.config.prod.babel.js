c8af7a36ade22bd281a9acb8d7c196bff1c5da2a
Oliver Turner <Oliver.Turner@Ticketmaster.co.uk>
2016-08-31 11:50:45 +0100

    Added dynamic docs generation to keep file references in sync across releases

--- config/webpack/webpack.config.prod.babel.js
+++ config/webpack/webpack.config.prod.babel.js
@@ -1,12 +1,16 @@
 import webpack from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 
+import {version} from '../../package.json';
 import {settings, getFilename} from './config';
 
 const config = settings({
+  devtool: false,
+
   output: {
     filename: 'web/' + getFilename('.min.js')
   },
-  devtool: false,
+
   plugins: [
     new webpack.LoaderOptionsPlugin({
       minimize: true,
@@ -16,6 +20,12 @@ const config = settings({
       compress:  {warnings: false},
       comments:  false,
       sourceMap: false
+    }),
+    new HtmlWebpackPlugin({
+      title:    'Ticketmaster SDK: ' + version,
+      filename: '../docs/web/index.html',
+      template: 'config/docs/index.html',
+      inject:   'head'
     })
   ]
 });
